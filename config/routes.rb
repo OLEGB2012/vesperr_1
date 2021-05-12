c5b07b874813fe1814265788374f2e4fde858b64
olb <olegb2012@gmail.com>
2021-05-12 21:18:32 +0300

    Configure mailer options

--- config/routes.rb
+++ config/routes.rb
@@ -23,4 +23,5 @@ Rails.application.routes.draw do
   resources :settings
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "visitors#index"
+  post 'visitors#submit_contact_form'
 end
